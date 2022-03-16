b9faae2600879e5bcf2ddffb2b18a847c85f4d6f
Aen <aen@Aens-MacBook-Pro.local>
2022-03-16 14:33:37 +0000

    Add concatenation of list with comma demliter.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 func main() {
 
@@ -8,15 +11,19 @@ func main() {
 
 func replaceDivisiblesby5(numbers []string) []string {
 	result := []string{}
-	for index := range numbers{
+	for index := range numbers {
 		number, err := strconv.Atoi(numbers[index])
 		if err == nil {
-			if number %5 == 0{
+			if number%5 == 0 {
 				result = append(result, "Buzz")
-			}else{
+			} else {
 				result = append(result, numbers[index])
 			}
 		}
 	}
 	return result
+}
+
+func GetCommaDelimitedString(input []string) string {
+	return strings.Join(input[:], ",")
 }
--- main_test.go
+++ main_test.go
@@ -23,7 +23,7 @@ func FizzBuzz3And5(list []int) []string {
 	return strList
 }
 
-func FizzBuzz3(list []int) []string{
+func FizzBuzz3(list []int) []string {
 	strList := make([]string, len(list))
 	for i, num := range list {
 		if num%3 == 0 {
@@ -46,4 +46,11 @@ func TestEvilFizzBuzz_FizzBuzz3And5(t *testing.T) {
 	var ExpectedList = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14",
 		"FizzBuzz", "16", "FizzBuzz", "FizzBuzz", "FizzBuzz", "FizzBuzz", "100"}
 	assert.Equal(t, ExpectedList, FizzBuzz3And5(list))
+}
+
+func TestGetCommaDelimitedString(t *testing.T) {
+	slice := []string{"1", "2", "Fizz"}
+	expectation := "1,2,Fizz"
+	actual := GetCommaDelimitedString(slice)
+	assert.Equal(t, expectation, actual)
 }
