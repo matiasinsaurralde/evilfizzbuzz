1bdeced2dd9ca6a89a88b15422d8759c87d1553c
demianfe <demianflorentin@gmail.com>
2022-03-16 11:21:30 -0300

    number generator added

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
 package main
 
+import "strconv"
+
+func GenerateNumbers(numbers []string) {
+	// generate numbers from 1 to 100
+	for i := range numbers {
+		numbers[i] = strconv.Itoa(i)
+	}
+
+}
+
 func main() {}
--- main_test.go
+++ main_test.go
@@ -1,4 +1,17 @@
 package main
 
-func TestHelloWorld(t *testing.T) {
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateNumbers(t *testing.T) {
+	numbers := make([]string, 101)
+	GenerateNumbers(numbers)
+
+	assert.Len(t, numbers, 101)
+	assert.Equal(t, "1", numbers[1])
+	assert.Equal(t, "50", numbers[50])
+	assert.Equal(t, "100", numbers[100])
 }
